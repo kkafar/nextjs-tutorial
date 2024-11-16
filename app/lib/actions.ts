cd1a21855a2eb947fb9781efd2efe7f79683df69
Kacper Kafara <kacperkafara@gmail.com>
2024-11-16 11:47:32 +0100

    Chapter 13: error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -17,44 +17,83 @@ const CreateInvoice = FormShema.omit({ id: true, date: true });
 const UpdateInvoice = FormShema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  });
+  let customerId, amount, status;
+  try {
+    const parseResult = CreateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+    customerId = parseResult.customerId;
+    amount = parseResult.amount;
+    status = parseResult.status;
+  } catch (error) {
+    return {
+      message: `Error while validating form data: ${error}`,
+    };
+  }
 
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date});
-  `;
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date});
+    `;
+  } catch (error) {
+    return {
+      message: `Database error: ${error}. Failed to create invoice`,
+    };
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  });
+  let customerId, amount, status;
+  try {
+    const parseResult = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+    customerId = parseResult.customerId;
+    amount = parseResult.amount;
+    status = parseResult.status;
+  } catch (error) {
+    return {
+      message: `Error while validating form data: ${error}`,
+    };
+  }
 
   const amountInCents = amount * 100;
 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id};
-  `;
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id};
+    `;
+  } catch (error) {
+    return {
+      message: `Database error: ${error}. Failed to update invoice`,
+    };
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id};`;
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id};`;
+  } catch (error) {
+    return {
+      message: `Database error: ${error}. Failed to delete invoice`,
+    };
+  }
+
   revalidatePath('/dashboard/invoices');
 }
